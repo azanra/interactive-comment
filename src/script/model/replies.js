f7b4781ae3d430ca0403c4f3daf341915dec3fe6
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-24 10:33:14 +0700

    Make replies inherit from comment with new attr

--- src/script/model/replies.js
+++ src/script/model/replies.js
@@ -1,8 +1,15 @@
 import Comment from "./comment.js";
 
 export default function Replies(attribute) {
-  this.comment = new Comment(attribute);
   this.id = attribute.id;
+  this.content = attribute.content;
+  this.createdAt = this.setCurrentDate();
+  this.score = attribute.score;
+  this.user = attribute.user;
+  this.replies = attribute.replies;
+  this.upvoted = null;
   this.type = "replies";
   this.replyingTo = attribute.replying;
 }
+
+Object.setPrototypeOf(Replies.prototype, Comment.prototype);
