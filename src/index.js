79fcc642f0cd29badcb99566b02ceb8a5fe74f01
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-25 11:00:33 +0700

    Add method to create object from the initial data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import "./style.css";
 import img from "./asset/resource/img.png";
+import { initialData } from "./script/model/data.js";
+import Comment from "./script/model/comment.js";
+import Replies from "./script/model/replies.js";
+import List from "./script/model/list.js";
 
 console.log("Hello world!");
 const paraElement = document.querySelector("p");
 const imgElement = document.createElement("img");
 imgElement.src = img;
 paraElement.appendChild(imgElement);
+
+const loadData = (function () {
+  const list = new List();
+  const initializeData = (data) => {
+    data.comments.map((item) => {
+      if (item.type === "comment") {
+        const comment = new Comment(item);
+        list.addData(comment);
+      } else if (item.type === "replies") {
+        const replies = new Replies(item);
+        list.addData(replies);
+      }
+    });
+  };
+  return { list, initializeData };
+})();
+
+loadData.initializeData(initialData);
+const data = loadData.list;
+console.log(data);
