862f2a0209db4719e755cea35dac5acc279b6a5d
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-27 09:24:46 +0700

    Add object to add new element to the dom

---
+++ src/script/view/element.js
@@ -0,0 +1,23 @@
+export const element = (function () {
+  const createDom = (attribute) => {
+    const parentElement = document.querySelector(attribute.parentElement);
+    const newElement = document.createElement(attribute.newElement);
+    setMultipleAttr(newElement, attribute.elementAttribute);
+    setTextContent(newElement, attribute.textContent);
+    parentElement.append(newElement);
+  };
+
+  const setMultipleAttr = (element, attribute) => {
+    for (const key in attribute) {
+      element.setAttribute(key, attribute[key]);
+    }
+  };
+
+  const setTextContent = (element, elementTextContent) => {
+    if (elementTextContent) {
+      element.textContent = elementTextContent;
+    }
+  };
+
+  return { createDom, setMultipleAttr, setTextContent };
+})();
