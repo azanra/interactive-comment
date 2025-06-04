ea45fc037da80f4870b9bd3c5f12d0fb34e54fff
azan-personal <112457939+azanra@users.noreply.github.com>
2025-06-05 06:49:58 +0700

    Add method create dom card and svg from the data

---
+++ src/script/view/card.js
@@ -0,0 +1,19 @@
+import { attribute } from "./attribute.js";
+import { element } from "./element.js";
+
+export const card = (function () {
+  const cardAttribute = attribute.card;
+  const svgAttribute = attribute.svg;
+
+  const createDom = (data) => {
+    for (const key in cardAttribute) {
+      const attribute = element.setUniqueId(cardAttribute[key], data.id);
+      element.createDom(attribute, data);
+    }
+    for (const key in svgAttribute) {
+      element.createSvgDom(svgAttribute[key], data.id);
+    }
+  };
+
+  return { createDom };
+})();
