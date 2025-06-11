69104552d1ac5a1be68b8fb15308153b5ebbe4d9
azan-personal <112457939+azanra@users.noreply.github.com>
2025-06-11 07:54:42 +0700

    Add user id to the constructor user object

--- src/script/model/user.js
+++ src/script/model/user.js
@@ -1,15 +1,20 @@
 import { initialData } from "./data.js";
 
-function User(username, image) {
+function User(id, username, image) {
   if (!new.target) {
     throw new Error("Use new to call the constructor object");
   }
+  this.id = id;
   this.username = username;
   this.image = image;
 }
 
 export const user = (function () {
   const currentUser = initialData.currentUser;
-  const activeUser = new User(currentUser.username, currentUser.image.png);
+  const activeUser = new User(
+    currentUser.id,
+    currentUser.username,
+    currentUser.image.png
+  );
   return { activeUser };
 })();
