a2c970b7f38a907013e0beae7acf260e030e16d2
azan-personal <112457939+azanra@users.noreply.github.com>
2025-06-11 07:52:25 +0700

    Adjust created date and id of the initial data

--- src/script/model/data.js
+++ src/script/model/data.js
@@ -5,6 +5,7 @@ import ramPng from "../../asset/resource/avatars/image-ramsesmiron.png";
 
 export const initialData = {
   currentUser: {
+    id: 1,
     image: {
       png: juliomoPng,
     },
@@ -12,10 +13,10 @@ export const initialData = {
   },
   comments: [
     {
-      id: 1,
+      id: 2,
       content:
         "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-      createdAt: "1 month ago",
+      createdAt: "Wed Aug 11 2025 07:42:06 GMT+0700 (Western Indonesia Time)",
       score: 12,
       user: {
         image: {
@@ -27,10 +28,10 @@ export const initialData = {
       type: "comment",
     },
     {
-      id: 2,
+      id: 3,
       content:
         "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-      createdAt: "2 weeks ago",
+      createdAt: "Wed Feb 17 2025 07:42:06 GMT+0700 (Western Indonesia Time)",
       score: 5,
       user: {
         image: {
@@ -38,34 +39,34 @@ export const initialData = {
         },
         username: "maxblagun",
       },
-      replies: [3, 4],
+      replies: [4, 5],
       type: "comment",
     },
     {
-      id: 3,
+      id: 4,
       content:
         "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-      createdAt: "1 week ago",
+      createdAt: "Wed Jun 01 2025 07:42:06 GMT+0700 (Western Indonesia Time)",
       score: 4,
       replyingTo: "maxblagun",
-      parent: 2,
+      parent: 3,
       user: {
         image: {
           png: ramPng,
         },
         username: "ramsesmiron",
       },
-      replies: [5],
+      replies: [6],
       type: "replies",
     },
     {
-      id: 4,
+      id: 5,
       content:
         "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-      createdAt: "2 days ago",
-      score: 2,
+      createdAt: "Wed Jun 07 2025 07:42:06 GMT+0700 (Western Indonesia Time)",
+      score: 3,
       replyingTo: "ramsesmiron",
-      parent: 2,
+      parent: 3,
       user: {
         image: {
           png: juliomoPng,
@@ -76,12 +77,12 @@ export const initialData = {
       type: "replies",
     },
     {
-      id: 5,
+      id: 6,
       content: "Agreed!!!",
-      createdAt: "1 week ago",
+      createdAt: "Wed Jun 10 2025 07:42:06 GMT+0700 (Western Indonesia Time)",
       score: 4,
       replyingTo: "ramsesmiron",
-      parent: 3,
+      parent: 4,
       user: {
         image: {
           png: amyPng,
