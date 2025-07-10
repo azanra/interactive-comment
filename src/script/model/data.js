530a61e82404080afeee4294813c4f7885d126e5
azan-personal <112457939+azanra@users.noreply.github.com>
2025-07-10 08:10:41 +0700

    Change the initial data structure

--- src/script/model/data.js
+++ src/script/model/data.js
@@ -5,15 +5,14 @@ import ramPng from "../../asset/resource/avatars/image-ramsesmiron.png";
 
 export const initialData = {
   currentUser: {
-    id: 1,
     image: {
       png: juliomoPng,
     },
     username: "juliusomo",
   },
-  comments: [
-    {
-      id: 2,
+  comments: {
+    1: {
+      id: 1,
       content:
         "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
       createdAt: "Wed Aug 11 2025 07:42:06 GMT+0700 (Western Indonesia Time)",
@@ -27,8 +26,8 @@ export const initialData = {
       replies: [],
       type: "comment",
     },
-    {
-      id: 3,
+    2: {
+      id: 2,
       content:
         "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
       createdAt: "Wed Feb 17 2025 07:42:06 GMT+0700 (Western Indonesia Time)",
@@ -42,8 +41,8 @@ export const initialData = {
       replies: [4, 5],
       type: "comment",
     },
-    {
-      id: 7,
+    3: {
+      id: 3,
       content: "Nice one",
       createdAt: "Wed Jun 15 2025 07:42:06 GMT+0700 (Western Indonesia Time)",
       score: 3,
@@ -56,7 +55,7 @@ export const initialData = {
       replies: [],
       type: "comment",
     },
-    {
+    4: {
       id: 4,
       content:
         "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
@@ -73,7 +72,7 @@ export const initialData = {
       replies: [6],
       type: "replies",
     },
-    {
+    5: {
       id: 5,
       content:
         "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
@@ -90,7 +89,7 @@ export const initialData = {
       replies: [],
       type: "replies",
     },
-    {
+    6: {
       id: 6,
       content: "Agreed!!!",
       createdAt: "Wed Jun 10 2025 07:42:06 GMT+0700 (Western Indonesia Time)",
@@ -106,5 +105,5 @@ export const initialData = {
       replies: [],
       type: "replies",
     },
-  ],
+  },
 };
