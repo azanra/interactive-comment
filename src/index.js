712bbed0cd48f8946741e37a29cee9d967406d4d
azan-personal <112457939+azanra@users.noreply.github.com>
2025-07-11 07:44:59 +0700

    Extract the render data into it own file

--- src/index.js
+++ src/index.js
@@ -1,53 +1,13 @@
 import "./style.css";
-import { card } from "./script/view/card.js";
 import { data } from "./script/mainData.js";
 import { renderAction } from "./script/view/renderAction.js";
 import commentCard, { commentAttr } from "./script/component/commentCard.js";
 import { initialData } from "./script/model/data.js";
+import renderData from "./script/renderData.js";
 
 // card.renderCard(data.list);
 // renderAction.renderDom(data.list[5]);
 // card.replyInputDom(activeUser, data.list[[2]]);
 
-const section = document.querySelector(".section");
-
 const comments = initialData.comments;
-
-const renderData = (function () {
-  const render = (dataToBeRendered) => {
-    const dataToBeRenderedArray = Object.values(dataToBeRendered);
-    dataToBeRenderedArray.forEach((data) => {
-      if (data.type === "comment") {
-        checkData(data, dataToBeRendered);
-      }
-    });
-  };
-
-  const checkData = (dataToCheck, dataToBeRendered) => {
-    const replies = dataToCheck.replies;
-    const parentComment = commentCard(commentAttr, dataToCheck);
-    if (replies.length > 0) {
-      replies.map((id) => {
-        checkNestedData(id, parentComment, dataToBeRendered);
-      });
-    }
-    section.appendChild(parentComment);
-  };
-
-  const checkNestedData = (id, parent, dataToBeRendered) => {
-    const data = dataToBeRendered[id];
-    const replies = data.replies;
-    const repliesComment = commentCard(commentAttr, data);
-    parent.appendChild(repliesComment);
-
-    if (replies.length > 0) {
-      replies.map((id) => {
-        checkNestedData(id, repliesComment, dataToBeRendered);
-      });
-    }
-  };
-
-  return { render };
-})();
-
 renderData.render(comments);
---
+++ src/script/renderData.js
@@ -0,0 +1,42 @@
+import commentCard, { commentAttr } from "./component/commentCard";
+
+const renderData = (function () {
+  const section = document.querySelector(".section");
+
+  const render = (dataToBeRendered) => {
+    const dataToBeRenderedArray = Object.values(dataToBeRendered);
+    dataToBeRenderedArray.forEach((data) => {
+      if (data.type === "comment") {
+        checkData(data, dataToBeRendered);
+      }
+    });
+  };
+
+  const checkData = (dataToCheck, dataToBeRendered) => {
+    const replies = dataToCheck.replies;
+    const parentComment = commentCard(commentAttr, dataToCheck);
+    if (replies.length > 0) {
+      replies.map((id) => {
+        checkNestedData(id, parentComment, dataToBeRendered);
+      });
+    }
+    section.appendChild(parentComment);
+  };
+
+  const checkNestedData = (id, parent, dataToBeRendered) => {
+    const data = dataToBeRendered[id];
+    const replies = data.replies;
+    const repliesComment = commentCard(commentAttr, data);
+    parent.appendChild(repliesComment);
+
+    if (replies.length > 0) {
+      replies.map((id) => {
+        checkNestedData(id, repliesComment, dataToBeRendered);
+      });
+    }
+  };
+
+  return { render };
+})();
+
+export default renderData;
