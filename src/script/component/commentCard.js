e1e5458572fde085a7b12ef4be0370c1bf175192
azan-personal <112457939+azanra@users.noreply.github.com>
2025-07-18 08:20:06 +0700

    Render comment header and body container

--- src/script/component/commentCard.js
+++ src/script/component/commentCard.js
@@ -15,6 +15,27 @@ const commentContentAttr = {
   },
 };
 
+const commentContainerAttr = {
+  elementType: "div",
+  elementAttribute: {
+    class: "commentContainer",
+  },
+};
+
+const commentHeaderAttr = {
+  elementType: "div",
+  elementAttribute: {
+    class: "commentHeader",
+  },
+};
+
+const commentBodyAttr = {
+  elementType: "div",
+  elementAttribute: {
+    class: "commentBody",
+  },
+};
+
 const commentCard = (commentAttr, data) => {
   const renderComment = () => {
     const comment = elementUtil.createDom(commentAttr);
@@ -25,7 +46,25 @@ const commentCard = (commentAttr, data) => {
   const renderCommentContent = () => {
     const commentContent = elementUtil.createDom(commentContentAttr);
     commentContent.appendChild(upvote(upvoteAttr, data));
+    commentContent.appendChild(renderCommentContainer());
     return commentContent;
+  };
+
+  const renderCommentContainer = () => {
+    const commentContainer = elementUtil.createDom(commentContainerAttr);
+    commentContainer.appendChild(renderCommentHeader());
+    commentContainer.appendChild(renderCommentBody());
+    return commentContainer;
+  };
+
+  const renderCommentHeader = () => {
+    const commentHeader = elementUtil.createDom(commentHeaderAttr);
+    return commentHeader;
+  };
+
+  const renderCommentBody = () => {
+    const commentBody = elementUtil.createDom(commentBodyAttr);
+    return commentBody;
   };
 
   return renderComment();
