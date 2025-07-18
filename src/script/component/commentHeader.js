4237c21f65128b571d73219e5f3375b5a981a179
azan-personal <112457939+azanra@users.noreply.github.com>
2025-07-18 09:03:59 +0700

    Render comment created at date

--- src/script/component/commentHeader.js
+++ src/script/component/commentHeader.js
@@ -1,3 +1,8 @@
+import {
+  formatDistanceToNow,
+  formatDistanceToNowStrict,
+  intervalToDuration,
+} from "date-fns";
 import { initialData } from "../model/data";
 import { elementUtil } from "./elementUtil";
 
@@ -30,6 +35,13 @@ const ownCommentMarkAttr = {
   textContent: "You",
 };
 
+const commentDateAttr = {
+  elementType: "p",
+  elementAttribute: {
+    class: "commentDate",
+  },
+};
+
 const commentHeader = (data) => {
   const renderProfileImg = () => {
     const img = elementUtil.createDom(imgAttr);
@@ -52,11 +64,19 @@ const commentHeader = (data) => {
     return document.createElement("span");
   };
 
+  const renderCommentDate = () => {
+    const commentDate = elementUtil.createDom(commentDateAttr);
+    const duration = formatDistanceToNowStrict(new Date(data.createdAt));
+    commentDate.textContent = `${duration} ago`;
+    return commentDate;
+  };
+
   const renderCommentHeader = () => {
     const commentHeader = elementUtil.createDom(commentHeaderAttr);
     commentHeader.appendChild(renderProfileImg());
     commentHeader.appendChild(renderProfileName());
     commentHeader.appendChild(renderOwnCommentMark());
+    commentHeader.appendChild(renderCommentDate());
     return commentHeader;
   };
 
