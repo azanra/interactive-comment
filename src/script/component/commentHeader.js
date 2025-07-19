78e11a70728ba158805415189b6caa1bfd832587
azan-personal <112457939+azanra@users.noreply.github.com>
2025-07-19 08:17:32 +0700

    Render delete action if it own comment

--- src/script/component/commentHeader.js
+++ src/script/component/commentHeader.js
@@ -1,8 +1,4 @@
-import {
-  formatDistanceToNow,
-  formatDistanceToNowStrict,
-  intervalToDuration,
-} from "date-fns";
+import { formatDistanceToNowStrict } from "date-fns";
 import { initialData } from "../model/data";
 import { elementUtil } from "./elementUtil";
 
@@ -42,7 +38,59 @@ const commentDateAttr = {
   },
 };
 
+const actionCommentContainerAttr = {
+  elementType: "div",
+  elementAttribute: {
+    class: "actionCommentContainerAttr",
+  },
+};
+
+const deleteActionContainerAttr = {
+  elementType: "div",
+  elementAttribute: {
+    class: "deleteActionContainerAttr",
+  },
+};
+
+const deleteActionBtnAttr = {
+  elementType: "button",
+  elementAttribute: {
+    type: "button",
+    class: "deleteActionBtn",
+  },
+  textContent: "Delete",
+};
+
+const deleteSvgAttr = {
+  uri: "http://www.w3.org/2000/svg",
+  name: "svg",
+  elementAttribute: {
+    width: "12",
+    height: "14",
+  },
+  xmlnsAttribute: {
+    nameSpace: "http://www.w3.org/2000/xmlns/",
+    name: "xmlns:xlink",
+    value: "http://www.w3.org/2000/svg",
+  },
+  path: {
+    uri: "http://www.w3.org/2000/svg",
+    name: "path",
+    elementAttribute: {
+      fill: "#ED6368",
+      d: "M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z",
+    },
+  },
+};
+
 const commentHeader = (data) => {
+  const IfDataIsActiveUser = () => {
+    const currentUser = JSON.parse(JSON.stringify(initialData.currentUser));
+    if (currentUser.username === data.user.username) {
+      return true;
+    }
+  };
+
   const renderProfileImg = () => {
     const img = elementUtil.createDom(imgAttr);
     img.src = data.user.image.png;
@@ -56,8 +104,7 @@ const commentHeader = (data) => {
   };
 
   const renderOwnCommentMark = () => {
-    const currentUser = JSON.parse(JSON.stringify(initialData.currentUser));
-    if (currentUser.username === data.user.username) {
+    if (IfDataIsActiveUser()) {
       const ownCommentMark = elementUtil.createDom(ownCommentMarkAttr);
       return ownCommentMark;
     }
@@ -71,12 +118,36 @@ const commentHeader = (data) => {
     return commentDate;
   };
 
+  const renderActionComment = () => {
+    const actionCommentContainer = elementUtil.createDom(
+      actionCommentContainerAttr,
+    );
+    if (IfDataIsActiveUser()) {
+      actionCommentContainer.appendChild(renderDeleteAction());
+      return actionCommentContainer;
+    } else {
+      return actionCommentContainer;
+    }
+  };
+
+  const renderDeleteAction = () => {
+    const deleteActionContainer = elementUtil.createDom(
+      deleteActionContainerAttr,
+    );
+    const deleteSvg = elementUtil.createSvgDom(deleteSvgAttr);
+    const deleteActionBtn = elementUtil.createDom(deleteActionBtnAttr);
+    deleteActionContainer.appendChild(deleteSvg);
+    deleteActionContainer.appendChild(deleteActionBtn);
+    return deleteActionContainer;
+  };
+
   const renderCommentHeader = () => {
     const commentHeader = elementUtil.createDom(commentHeaderAttr);
     commentHeader.appendChild(renderProfileImg());
     commentHeader.appendChild(renderProfileName());
     commentHeader.appendChild(renderOwnCommentMark());
     commentHeader.appendChild(renderCommentDate());
+    commentHeader.appendChild(renderActionComment());
     return commentHeader;
   };
 
