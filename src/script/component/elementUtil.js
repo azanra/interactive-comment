8221aeb1cae7769701a5c8aa69fb92281f6d7e8d
azan-personal <112457939+azanra@users.noreply.github.com>
2025-07-20 12:41:18 +0700

    Remove unused code

--- src/script/component/elementUtil.js
+++ src/script/component/elementUtil.js
@@ -19,39 +19,6 @@ export const elementUtil = (function () {
     }
   };
 
-  const setUniqueId = (attribute, id) => {
-    if (id) {
-      const copyAttribute = JSON.parse(JSON.stringify(attribute));
-      copyAttribute.elementAttribute.id = `${copyAttribute.elementAttribute.class}-${id}`;
-      return copyAttribute;
-    } else {
-      return attribute;
-    }
-  };
-
-  const appendReferenceParent = (attribute) => {
-    if (attribute?.withParentId === true) {
-      attribute.referenceParent = "withParentId";
-    } else if (attribute.uniqueParent === true) {
-      attribute.referenceParent = "withDataId";
-    } else {
-      attribute.referenceParent = "none";
-    }
-    return attribute;
-  };
-
-  const referenceElement = (attribute, data) => {
-    if (attribute.referenceParent === "withParentId") {
-      return document.querySelector(
-        `${attribute.parentElement}-${data.parent}`,
-      );
-    } else if (attribute.referenceParent === "withDataId") {
-      return document.querySelector(`${attribute.parentElement}-${data.id}`);
-    } else if (attribute.referenceParent === "none") {
-      return document.querySelector(attribute.parentElement);
-    }
-  };
-
   const createSvgDom = (attribute) => {
     const { uri, name, elementAttribute, xmlnsAttribute, path } = attribute;
     const svgElement = document.createElementNS(uri, name);
@@ -67,51 +34,11 @@ export const elementUtil = (function () {
     return svgElement;
   };
 
-  const createImg = (attribute, src) => {
-    if (attribute.hasOwnProperty("isImage")) {
-      const attrWithSrc = JSON.parse(JSON.stringify(attribute));
-      attrWithSrc.elementAttribute.src = src;
-      return attrWithSrc;
-    } else {
-      return attribute;
-    }
-  };
-
-  const populateUniqueText = (attribute, data) => {
-    if (attribute.hasOwnProperty("dataText")) {
-      const uniqueTextAttr = JSON.parse(JSON.stringify(attribute));
-      uniqueTextAttr.textContent = nestedTextContentValue(attribute, data);
-      return uniqueTextAttr;
-    } else {
-      return attribute;
-    }
-  };
-
-  const renderAtToReplying = (data) => {
-    if (data.type === "replies") {
-      const replyingTo = document.querySelectorAll(".dataReplyingTo");
-      replyingTo.forEach((reply) => {
-        if (reply.textContent && !reply.textContent.includes("@")) {
-          reply.textContent = `@${reply.textContent}`;
-        }
-      });
-    }
-  };
-
-  const nestedTextContentValue = (attribute, data) => {
-    if (attribute.dataText.includes("-")) {
-      const dataText = attribute.dataText.split("-");
-      return data[`${dataText[0]}`][`${dataText[1]}`];
-    } else {
-      return data[attribute.dataText];
-    }
-  };
-
-  const renderCreatedSince = (attribute, data) => {
-    if (attribute.elementAttribute.class === "createdAt") {
-      const attrWithCreatedSince = JSON.parse(JSON.stringify(attribute));
-      attrWithCreatedSince.textContent = data.renderDate();
-      return attrWithCreatedSince;
+  const setUniqueId = (attribute, id) => {
+    if (id) {
+      const copyAttribute = JSON.parse(JSON.stringify(attribute));
+      copyAttribute.elementAttribute.id = `${copyAttribute.elementAttribute.class}-${id}`;
+      return copyAttribute;
     } else {
       return attribute;
     }
@@ -119,8 +46,8 @@ export const elementUtil = (function () {
 
   return {
     createDom,
-    setUniqueId,
     createSvgDom,
     setTextContent,
+    setUniqueId,
   };
 })();
